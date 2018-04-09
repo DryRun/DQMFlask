f179d3d56f5fc8f44bd8112ae64a00a61f820e45
DryRun <david.renhwa.yu@gmail.com>
2018-04-09 18:22:12 +0200

    Debugging done

--- dqmdata/hcal_local/models.py
+++ dqmdata/hcal_local/models.py
@@ -157,7 +157,10 @@ def check_overwrite(quantity, run, emap_version, overwrite):
 			print "[check_overwrite] ERROR : Run {} already exists in DB for quantity {}! Specify overwrite to overwrite.".format(run, quantity)
 			return False
 		else:
+			print "[check_overwrite] INFO : Deleting existing entries for run {}, quantity {}".format(run, quantity)
 			for reading in q.all():
+				print "[check_overwrite] INFO : Deleting ",
+				print reading
 				db.session.delete(reading)
 			db.session.commit()
 	return True
