e1626b92d43a243392e4fc6eea8602f48bbc9fa1
DryRun <david.renhwa.yu@gmail.com>
2018-05-03 17:59:36 +0200

    @app decorators inside create_app

--- dqmdata/__init__.py
+++ dqmdata/__init__.py
@@ -5,26 +5,6 @@ from flask_migrate import Migrate
 # Import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 
-# Sample HTTP error handling
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('templates/404.html'), 404
-
-# Import a module / component using its blueprint handler variable (hcal_local)
-from dqmdata.common.controllers import common as common
-from dqmdata.hcal_local.controllers import hcal_local as hcal_local
-
-
-@app.route('/')
-@app.route('/index.html')
-def index():
-	return '''
-<html>
-	<body>
-		Welcome to HCAL DQMFlask, written by David Yu. This is only a backend API, not a website!
-	</body>
-</html>'''
-
 def create_app():
 	# Define the WSGI application object
 	app = Flask(__name__)
@@ -42,6 +22,26 @@ def create_app():
 	app.register_blueprint(hcal_local)
 	# app.register_blueprint(xyz_module)
 	# ..
+
+	# Sample HTTP error handling
+	@app.errorhandler(404)
+	def not_found(error):
+	    return render_template('templates/404.html'), 404
+
+	# Import a module / component using its blueprint handler variable (hcal_local)
+	from dqmdata.common.controllers import common as common
+	from dqmdata.hcal_local.controllers import hcal_local as hcal_local
+
+
+	@app.route('/')
+	@app.route('/index.html')
+	def index():
+		return '''
+	<html>
+		<body>
+			Welcome to HCAL DQMFlask, written by David Yu. This is only a backend API, not a website!
+		</body>
+	</html>'''
 
 
 # Build the database:
