1a1b63b15abf84af3359171425802566125f5c90
DryRun <david.renhwa.yu@gmail.com>
2018-05-03 18:03:18 +0200

    Try again

--- dqmdata/__init__.py
+++ dqmdata/__init__.py
@@ -5,42 +5,41 @@ from flask_migrate import Migrate
 # Import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 
-def create_app():
-	# Import a module / component using its blueprint handler variable (hcal_local)
-	from dqmdata.common.controllers import common as common
-	from dqmdata.hcal_local.controllers import hcal_local as hcal_local
+# Define the WSGI application object
+app = Flask(__name__)
 
-	# Define the WSGI application object
-	app = Flask(__name__)
+# Configurations
+app.config.from_object('config')
 
-	# Configurations
-	app.config.from_object('config')
+# Define the database object which is imported
+# by modules and controllers
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-	# Define the database object which is imported
-	# by modules and controllers
-	db = SQLAlchemy(app)
-	migrate = Migrate(app, db)
+# Register blueprint(s)
+# Import a module / component using its blueprint handler variable (hcal_local)
+from dqmdata.common.controllers import common as common
+from dqmdata.hcal_local.controllers import hcal_local as hcal_local
 
-	# Register blueprint(s)
-	app.register_blueprint(common)
-	app.register_blueprint(hcal_local)
-	# app.register_blueprint(xyz_module)
-	# ..
+app.register_blueprint(common)
+app.register_blueprint(hcal_local)
+# app.register_blueprint(xyz_module)
+# ..
 
-	# Sample HTTP error handling
-	@app.errorhandler(404)
-	def not_found(error):
-	    return render_template('templates/404.html'), 404
+# Sample HTTP error handling
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('templates/404.html'), 404
 
-	@app.route('/')
-	@app.route('/index.html')
-	def index():
-		return '''
-	<html>
-		<body>
-			Welcome to HCAL DQMFlask, written by David Yu. This is only a backend API, not a website!
-		</body>
-	</html>'''
+@app.route('/')
+@app.route('/index.html')
+def index():
+	return '''
+<html>
+	<body>
+		Welcome to HCAL DQMFlask, written by David Yu. This is only a backend API, not a website!
+	</body>
+</html>'''
 
 
 # Build the database:
@@ -48,7 +47,7 @@ def create_app():
 #db.create_all() 
 
 def wsgi(*args, **kwargs):
-    return create_app()(*args, **kwargs)
+    return app
 
 if __name__ == "__main__":
-	create_app().run()
+	app.run()
