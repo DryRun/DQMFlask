8fee203bcfb6a364bb001844032480f339d3bff7
DryRun <david.renhwa.yu@gmail.com>
2018-06-27 16:26:21 +0200

    Python module touchup

--- dqmdata/common/controllers.py
+++ dqmdata/common/controllers.py
@@ -2,6 +2,6 @@
 from flask import Blueprint, request, render_template, \
                   flash, g, session, redirect, url_for, current_app
 from dqmdata import db
-from dqmdata.common.models.run import LocalRun
+from dqmdata.common.models.local_run import LocalRun
 
 common = Blueprint('common', __name__, url_prefix='/common')
