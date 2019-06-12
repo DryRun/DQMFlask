13d7722bde91341962a23239733633528c2a2efc
DryRun <david.renhwa.yu@gmail.com>
2019-06-12 16:05:21 -0500

    Adding some dependencies for flask

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=['dqmdata'],
     include_package_data=True,
     install_requires=[
-        'flask', 'flask-sqlalchemy', 'flask_migrate', 'requests'
+        'flask', 'flask-sqlalchemy', 'flask_migrate', 'requests', 'bs4'
     ],
 )
