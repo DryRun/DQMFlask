4fc2e1822e08cd9c3634d8e1689965fa74fef478
DryRun <david.renhwa.yu@gmail.com>
2019-06-12 16:02:34 -0500

    Adding some config files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='hdqm',
+    packages=['dqmdata'],
+    include_package_data=True,
+    install_requires=[
+        'flask', 'flask-sqlalchemy', 'flask_migrate'
+    ],
+)
